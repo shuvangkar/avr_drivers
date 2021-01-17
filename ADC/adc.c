179f4c494d2501164a1196294b0688434595e40c
shuvangkar <sshuvo93@gmail.com>
2021-01-18 01:18:20 +0600

    adc begin and trig source added

--- ADC/adc.c
+++ ADC/adc.c
@@ -1,4 +1,31 @@
 #include "adc.h"
 
-
 void (*_isrCb)();
+
+void adcBegin(adc_ref_t ref)
+{
+    ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); //Prescaler 128.So F_adc = 16MHz/128 = 125KHz
+    ADMUX |= (uintr_t)ref;
+    ADCSRA |= (1 << ADEN);
+}
+
+void adcSetAutoTriggerSource(adc_trig_source_t trigSource)
+{
+    uint8_t source = (uint8_t)trigSource;
+    if (source)
+    {
+        ADCSRA |= (1 << ADATE); //ADC Auto trigger enable
+    }
+    ADCSRB &= 0b11111000; //Clearing trigger source bits
+    ADCSRB |= source;
+}
+
+void adcAttachInterrrupt(void(*adc_isr));
+void adcDetachInterrupt();
+void adcSetChannel(uint8_t channel);
+
+void adcStartConversion(uint8_t channel);
+
+uint16_t adcRead(uint8_t channel);
+float adcReadAverage(uint8_t channel, uint8_t sample);
+float adcReadAvcc();
--- ADC/adc.h
+++ ADC/adc.h
@@ -8,14 +8,30 @@ extern "C" {
 #include <stdbool.h>
 #include <stddef.h>
 
+typedef enum adc_ref_t
+{
+    AREF    = 0<<REFS0,
+    AVCC    = 1<<REFS0,
+    BANDGAP = 3<<REFS0
+}adc_ref_t;
 
-void adcBegin();
+typedef enum adc_trig_source_t
+{
+    FREE_RUNNING = 0b000,
+    TIMER1_CMPB  = 0b101
+}adc_trig_source_t;
+
+
+void adcBegin(adc_ref_t ref);
 void adcAttachInterrrupt(void (*adc_isr));
 void adcDetachInterrupt();
 void adcSetChannel(uint8_t channel);
-void adcSetAutoTriggerSource(uint8_t source);
+void adcSetAutoTriggerSource(adc_trig_source_t trigSource);
 void adcStartConversion(uint8_t channel);
 
+uint16_t adcRead(uint8_t channel);
+float adcReadAverage(uint8_t channel, uint8_t sample);
+float adcReadAvcc();
 
 
 
